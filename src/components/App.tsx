71e8fd0cdb736a8e3d32aa4d01b11cc50ffc6968
unknown <mikolajdobisz@gmail.com>
2020-08-23 18:04:03 +0200

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/style.scss';
+
+export const App = () => {
+
+	const greetings : string = "Hello Friend!";
+
+	return (
+		<div>
+			{greetings}
+		</div>
+	)
+}
